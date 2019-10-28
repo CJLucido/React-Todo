098005ee7383e35eb8a4f7da8d86f4d8d24bd171
CJ Lucido <c.jose.lucido@gmail.com>
2019-10-28 13:03:45 -0700

    added ability to type in todoForm input

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -1,3 +1,47 @@
-import React from 'react'
+import React, {Component} from 'react'
+
+class TodoForm extends Component{
+    constructor(){
+        super();
+        this.state={
+            newItem: ""
+        }
+    }
+
+
+    handleChange = e => {
+        this.setState({
+            newItem: e.target.value
+        })
+    }
+
+    handleSubmit = e => {
+        e.preventDefault();
+        console.log('it\'s working!!!')
+    }
+
+    render(){
+        return(
+            <form onSubmit={this.handleSubmit}>
+                <label htmlFor='item'/>
+                <input type='text'
+                name="item"
+                id="item"
+                value={this.state.newItem}
+                onChange={this.handleChange}
+                />
+                <button>Add</button>
+            
+            
+            
+            </form>
+        )
+    }
+
+
+
+}
+
+
 
 export default TodoForm
