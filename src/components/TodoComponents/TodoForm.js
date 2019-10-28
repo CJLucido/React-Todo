76a4872ca2a20a6a669589378bf183a9efb6e9b6
CJ Lucido <c.jose.lucido@gmail.com>
2019-10-28 13:08:16 -0700

    added second form for clear completed button

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -20,21 +20,30 @@ class TodoForm extends Component{
         console.log('it\'s working!!!')
     }
 
+    handleClear = e => {
+        e.preventDefault();
+        console.log('it\'s also working!!!')
+    }
+
+
     render(){
         return(
-            <form onSubmit={this.handleSubmit}>
-                <label htmlFor='item'/>
-                <input type='text'
-                name="item"
-                id="item"
-                value={this.state.newItem}
-                onChange={this.handleChange}
-                />
-                <button>Add</button>
-            
-            
-            
-            </form>
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label htmlFor='item'/>
+                    <input type='text'
+                    name="item"
+                    id="item"
+                    value={this.state.newItem}
+                    onChange={this.handleChange}
+                    />
+                    <button>Add</button>
+                </form>
+                <form onSubmit={this.handleClear}>
+                    <button>Clear Completed</button>
+                
+                </form>
+            </div>
         )
     }
 
