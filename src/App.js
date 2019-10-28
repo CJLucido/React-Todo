089d027427566e763a391fafccae8e2a6569d9c3
CJ Lucido <c.jose.lucido@gmail.com>
2019-10-28 12:55:19 -0700

    began defining TodoList and Todo, need to make style classes for Todo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
-
+import TodoList from "./components/TodoComponents/TodoList"
+import TodoForm from "./components/TodoComponents/TodoForm"
 
 const data = [
   {
@@ -35,15 +36,35 @@ class App extends React.Component {
   constructor(){
     super();
     this.state = {
-
+      todos: data
     }
   }
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+  handleIt = () => {
+
+  }
+
+  handleAddItem = (task) => {
+    const newItem = {
+      task: task,
+      id: Date.now(),
+      completed: false
+    };
+
+
+    this.setState({
+      todos: [...this.state.todos, newItem]
+    })
+  }
+
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <TodoForm/>
+        <TodoList todos={this.state.todos}/>
       </div>
     );
   }
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,24 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from "react"
+
+import Todo from "./Todo"
+
+ function TodoList(props){
+
+    return (
+        <>
+            {props.todos.map(item => (
+                <Todo
+                    key={item.id}
+                    item={item}
+                />
+                                     )
+                            )
+            }
+
+        </>
+    )
+}
+
+export default TodoList
